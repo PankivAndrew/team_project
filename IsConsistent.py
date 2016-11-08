f709369bc50521e32c09464d84b65fd6a6ca66b1
PankivAndrew <pankiv_a@ucu.edu.ua>
2016-11-08 14:58:06 +0200

    Add files via upload

---
+++ IsConsistent.py
@@ -0,0 +1,12 @@
+def consistOFzero(vector):
+    OFzero = True
+    for i in vector:
+        if i != 0:
+            OFzero = False
+            break;
+    return OFzero
+def numberOFrows(V):
+    return len(V)
+def numberOf
+def ToReducedRowEchelonForm(M):
+    Lead = 0
